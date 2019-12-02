2e97938f7afbe248f4dc263adc5974b3dfab07e9
holydk <omigoneo@gmail.com>
2019-12-02 11:48:41 +0300

    Issue 4087 Optimize DeleteGuests sp

--- upgradescripts/4.20-4.30 (under development)/upgrade.sql
+++ upgradescripts/4.20-4.30 (under development)/upgrade.sql
@@ -292,3 +292,73 @@ BEGIN
 	Longitude decimal(18, 8) NULL
 END
 GO
+
+-- update the "DeleteGuests" stored procedure
+ALTER PROCEDURE [dbo].[DeleteGuests]
+(
+	@OnlyWithoutShoppingCart bit = 1,
+	@CreatedFromUtc datetime,
+	@CreatedToUtc datetime,
+	@TotalRecordsDeleted int = null OUTPUT
+)
+AS
+BEGIN
+	CREATE TABLE #tmp_guests (CustomerId int)
+		
+	INSERT #tmp_guests (CustomerId)
+	SELECT [Id] FROM [Customer] c with (NOLOCK)
+	WHERE
+	--created from
+	((@CreatedFromUtc is null) OR (c.[CreatedOnUtc] > @CreatedFromUtc))
+	AND
+	--created to
+	((@CreatedToUtc is null) OR (c.[CreatedOnUtc] < @CreatedToUtc))
+	AND
+	--shopping cart items
+	((@OnlyWithoutShoppingCart=0) OR (NOT EXISTS(SELECT 1 FROM [ShoppingCartItem] sci with (NOLOCK) where sci.[CustomerId]=c.[Id])))
+	AND
+	--guests only
+	(EXISTS(SELECT 1 FROM [Customer_CustomerRole_Mapping] ccrm with (NOLOCK) inner join [CustomerRole] cr with (NOLOCK) on cr.[Id]=ccrm.[CustomerRole_Id] WHERE ccrm.[Customer_Id]=c.[Id] AND cr.[SystemName] = N'Guests'))
+	AND
+	--no orders
+	(NOT EXISTS(SELECT 1 FROM [Order] o with (NOLOCK) WHERE o.[CustomerId]=c.[Id]))
+	AND
+	--no blog comments
+	(NOT EXISTS(SELECT 1 FROM [BlogComment] bc with (NOLOCK) WHERE bc.[CustomerId]=c.[Id]))
+	AND
+	--no news comments
+	(NOT EXISTS(SELECT 1 FROM [NewsComment] nc with (NOLOCK) WHERE nc.[CustomerId]=c.[Id]))
+	AND
+	--no product reviews
+	(NOT EXISTS(SELECT 1 FROM [ProductReview] pr with (NOLOCK) WHERE pr.[CustomerId]=c.[Id]))
+	AND
+	--no product reviews helpfulness
+	(NOT EXISTS(SELECT 1 FROM [ProductReviewHelpfulness] prh with (NOLOCK) WHERE prh.[CustomerId]=c.[Id]))
+	AND
+	--no poll voting
+	(NOT EXISTS(SELECT 1 FROM [PollVotingRecord] pvr with (NOLOCK) WHERE pvr.[CustomerId]=c.[Id]))
+	AND
+	--no forum topics 
+	(NOT EXISTS(SELECT 1 FROM [Forums_Topic] ft with (NOLOCK) WHERE ft.[CustomerId]=c.[Id]))
+	AND
+	--no forum posts 
+	(NOT EXISTS(SELECT 1 FROM [Forums_Post] fp with (NOLOCK) WHERE fp.[CustomerId]=c.[Id]))
+	AND
+	--no system accounts
+	(c.IsSystemAccount = 0)
+	
+	--delete guests
+	DELETE [Customer]
+	WHERE [Id] IN (SELECT [CustomerId] FROM #tmp_guests)
+	
+	--delete attributes
+	DELETE [GenericAttribute]
+	WHERE ([EntityId] IN (SELECT [CustomerId] FROM #tmp_guests))
+	AND
+	([KeyGroup] = N'Customer')
+	
+	--total records
+	SELECT @TotalRecordsDeleted = COUNT(1) FROM #tmp_guests
+	
+	DROP TABLE #tmp_guests
+END
